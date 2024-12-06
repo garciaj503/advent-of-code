f2f0a23b5ce0d4337c9a26e1ed4d6dd266174334
Jaime Garcia <jaimegarcia@Jaimes-MacBook-Air.local>
2024-12-05 21:24:40 -0800

    Day one

---
+++ day1.py
@@ -0,0 +1,37 @@
+import math
+
+with open("inputs.txt") as file:
+    data = file.read()
+# Process the data to create two lists
+first_column = []
+second_column = []
+
+# Split the data into lines and then into columns
+for line in data.strip().split("\n"):
+    col1, col2 = line.split()
+    first_column.append(int(col1))
+    second_column.append(int(col2))
+
+first_column.sort()
+second_column.sort()
+
+total_sum = 0
+i = 0
+while i < len(first_column):
+    total_sum += abs(first_column[i] - second_column[i])
+    i += 1
+
+print(total_sum)
+
+similarity = 0
+for value in first_column:
+    how_many_times = second_column.count(value)
+    similarity += value * how_many_times
+
+print(similarity)
+
+
+
+
+   
+
